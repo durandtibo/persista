04de939e11a65096a98c0568682b8bb0a0fa7dcb
Thibaut Durand <durand.tibo@gmail.com>
2026-07-15 23:51:57 -0700

    Add `is_duckdb_available`

---
+++ src/persista/utils/imports/__init__.py
@@ -0,0 +1,17 @@
+r"""Contain utilities for optional dependencies."""
+
+from __future__ import annotations
+
+__all__ = [
+    "check_duckdb",
+    "duckdb_available",
+    "is_duckdb_available",
+    "raise_duckdb_missing_error",
+]
+
+from persista.utils.imports.duckdb import (
+    check_duckdb,
+    duckdb_available,
+    is_duckdb_available,
+    raise_duckdb_missing_error,
+)
