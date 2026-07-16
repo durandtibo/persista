cd829c20f0dd5bcffdbd778d67e1ec18432b7210
Thibaut Durand <durand.tibo@gmail.com>
2026-07-15 23:59:39 -0700

    Add `is_faker_available`

--- src/persista/utils/imports/__init__.py
+++ src/persista/utils/imports/__init__.py
@@ -4,9 +4,13 @@ from __future__ import annotations
 
 __all__ = [
     "check_duckdb",
+    "check_faker",
     "duckdb_available",
+    "faker_available",
     "is_duckdb_available",
+    "is_faker_available",
     "raise_duckdb_missing_error",
+    "raise_faker_missing_error",
 ]
 
 from persista.utils.imports.duckdb import (
@@ -14,4 +18,10 @@ from persista.utils.imports.duckdb import (
     duckdb_available,
     is_duckdb_available,
     raise_duckdb_missing_error,
+)
+from persista.utils.imports.faker import (
+    check_faker,
+    faker_available,
+    is_faker_available,
+    raise_faker_missing_error,
 )
