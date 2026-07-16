ce6e87f1aff41566c9f9118c85b1b843ebe5e659
Thibaut Durand <durand.tibo@gmail.com>
2026-07-16 15:19:57 -0700

    Add `validate_batch_size`

--- src/persista/store/__init__.py
+++ src/persista/store/__init__.py
@@ -2,8 +2,18 @@ r"""Contain stores."""
 
 from __future__ import annotations
 
-__all__ = ["BaseStore", "OnConflict", "normalize_on_conflict", "validate_on_conflict"]
+__all__ = [
+    "BaseStore",
+    "OnConflict",
+    "normalize_on_conflict",
+    "validate_batch_size",
+    "validate_on_conflict",
+]
 
 from persista.store.base import BaseStore
 from persista.store.types import OnConflict
-from persista.store.validation import normalize_on_conflict, validate_on_conflict
+from persista.store.validation import (
+    normalize_on_conflict,
+    validate_batch_size,
+    validate_on_conflict,
+)
