129f0c9ccc99031fc1640ce6099cc0e03725330d
Thibaut Durand <durand.tibo@gmail.com>
2026-07-16 15:40:07 -0700

    Add `SQLiteStore` (#21)

--- src/persista/store/__init__.py
+++ src/persista/store/__init__.py
@@ -3,11 +3,11 @@ r"""Contain stores."""
 from __future__ import annotations
 
 __all__ = [
+    "BaseSQLiteStore",
     "BaseStore",
     "InMemoryStore",
     "OnConflict",
-    "normalize_on_conflict",
-    "OnConflict",
+    "SQLiteStore",
     "normalize_on_conflict",
     "validate_batch_size",
     "validate_on_conflict",
@@ -15,6 +15,7 @@ __all__ = [
 
 from persista.store.base import BaseStore
 from persista.store.in_memory import InMemoryStore
+from persista.store.sqlite import BaseSQLiteStore, SQLiteStore
 from persista.store.types import OnConflict
 from persista.store.validation import (
     normalize_on_conflict,
