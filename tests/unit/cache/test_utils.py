2cefe9cf561c53061dd197547c7ff6ba40b587fb
Thibaut Durand <durand.tibo@gmail.com>
2026-07-20 03:13:15 -0700

    Improve `make_key` (#102)

--- tests/unit/cache/test_utils.py
+++ tests/unit/cache/test_utils.py
@@ -48,3 +48,44 @@ def test_make_key_non_json_serializable_argument() -> None:
 
     with pytest.raises(TypeError):
         make_key("func", (Custom(),), {})
+
+
+def test_make_key_non_json_serializable_argument_ignored() -> None:
+    class Custom:
+        pass
+
+    assert make_key("func", (Custom(),), {}, ignore_non_serializable=True) == make_key(
+        "func", (), {}, ignore_non_serializable=True
+    )
+
+
+def test_make_key_non_json_serializable_kwarg_ignored() -> None:
+    class Custom:
+        pass
+
+    assert make_key(
+        "func", (), {"a": 1, "obj": Custom()}, ignore_non_serializable=True
+    ) == make_key("func", (), {"a": 1}, ignore_non_serializable=True)
+
+
+def test_make_key_ignore_non_serializable_keeps_serializable_args() -> None:
+    assert make_key("func", (1, 2), {}, ignore_non_serializable=True) == make_key(
+        "func", (1, 2), {}
+    )
+
+
+def test_make_key_ignore_non_serializable_different_serializable_values() -> None:
+    class Custom:
+        pass
+
+    assert make_key("func", (1, Custom()), {}, ignore_non_serializable=True) != make_key(
+        "func", (2, Custom()), {}, ignore_non_serializable=True
+    )
+
+
+def test_make_key_ignore_non_serializable_default_false() -> None:
+    class Custom:
+        pass
+
+    with pytest.raises(TypeError):
+        make_key("func", (), {"obj": Custom()})
