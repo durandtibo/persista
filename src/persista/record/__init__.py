d68a912f6c3f308a9d5b04b29ff5e722929000a0
Thibaut Durand <durand.tibo@gmail.com>
2026-09-06 20:03:09 -0700

    Add record package (#226)

---
+++ src/persista/record/__init__.py
@@ -0,0 +1,19 @@
+r"""Define records."""
+
+from __future__ import annotations
+
+__all__ = [
+    "Record",
+    "filter_by_metadata",
+    "filter_by_metadata_range",
+    "filter_by_metadata_values",
+    "sort_by_metadata",
+]
+
+from persista.record.filter import (
+    filter_by_metadata,
+    filter_by_metadata_range,
+    filter_by_metadata_values,
+)
+from persista.record.record import Record
+from persista.record.sort import sort_by_metadata
