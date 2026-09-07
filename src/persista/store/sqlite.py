484ff2c04dd2e215631b0377b0a73d82b5829260
Thibaut Durand <durand.tibo@gmail.com>
2026-09-06 23:05:40 -0700

    Improve tests

--- src/persista/store/sqlite.py
+++ src/persista/store/sqlite.py
@@ -161,7 +161,15 @@ class BaseSQLiteStore(BaseStore, MultilineDisplayMixin):
         if not self._closed:
             return
         self._conn = self._connect()
-        self._ensure_schema()
+        try:
+            self._ensure_schema()
+        except BaseException:
+            # Don't leak the connection just opened above: __enter__ never
+            # calls __exit__/close() when it raises, so this is the only
+            # chance to close it before it's garbage-collected.
+            self._conn.close()
+            self._conn = None
+            raise
         self._aschema_ready = False
         self._closed = False
 
